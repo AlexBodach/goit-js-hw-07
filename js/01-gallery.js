614988f06bf59919579a35a2f2af88b94d8387d7
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2021-12-10 21:49:44 +0200

    Made task 1.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const ref = {
+    boxGallery: document.querySelector("div.gallery")
+}
 
-console.log(galleryItems);
+const gallery = galleryItems.map((element) => 
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${element.original}">
+            <img
+                class="gallery__image"
+                src="${element.preview}"
+                data-source="large-image.jp"
+                alt="${element.description}"
+            />
+        </a>
+    </div>`   
+).join(''); 
+
+ref.boxGallery.insertAdjacentHTML('beforeend', gallery);
+
