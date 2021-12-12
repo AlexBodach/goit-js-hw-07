2fe5989c168678fbd7822a944e387bf3c4b81b36
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2021-12-12 21:07:37 +0200

    completed task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const ref = {
+    boxGallery: document.querySelector(".gallery")
+}
+
+const galleryStart = galleryItems.map((element) => 
+    `<a class="gallery__item" href="${element.original}">
+        <img class="gallery__image" src="${element.preview}" 
+        title="${element.description}" alt="${element.description}" />
+        </a>`   
+).join(''); 
+
+ref.boxGallery.insertAdjacentHTML('beforeend', galleryStart);
+let gallery = new SimpleLightbox('.gallery a', 
+{ captionPosition: "bottom", captionDelay: 250 } );
+
+gallery.on('show.simplelightbox', function (e) {});
