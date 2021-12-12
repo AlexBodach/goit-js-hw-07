7b2b2cdbadd923eb27941c8882d08d8846037f25
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2021-12-12 21:25:04 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,12 +7,13 @@ const ref = {
 const galleryStart = galleryItems.map((element) => 
     `<a class="gallery__item" href="${element.original}">
         <img class="gallery__image" src="${element.preview}" 
-        title="${element.description}" alt="${element.description}" />
+         alt="${element.description}" />
         </a>`   
 ).join(''); 
 
 ref.boxGallery.insertAdjacentHTML('beforeend', galleryStart);
 let gallery = new SimpleLightbox('.gallery a', 
-{ captionPosition: "bottom", captionDelay: 250 } );
+{ captionsData: "alt", captionPosition: "bottom", captionDelay: 250 } );
+
 
 gallery.on('show.simplelightbox', function (e) {});
